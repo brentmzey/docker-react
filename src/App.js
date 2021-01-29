93835d266c37c4692df7ab01d1107367d1bcd144
Brent Zey <brentmzey4795@gmail.com>
2021-01-29 13:38:13 -0600

    change App.js French greeting

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Bienvenu mes amis!!!</p>
+        <p>Bienvenu mon amour!!! Ceci c'est la seule difference.</p>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
